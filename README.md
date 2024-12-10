4a498e633bfbf542c29c86131fd22ff9e2fed413
Dan Whitehouse <drw@dim-sum.org>
2024-12-10 16:56:09 +0000

    making some slow progress...

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # multirpg-db
+
+sudo -u postgres psql
+postgres=# create database multirpg;
+postgres=# create user myuser with encrypted password 'mypass';
+postgres=# grant all privileges on database multirpg to myuser;
--- stats.py
+++ stats.py
@@ -1,16 +1,59 @@
 #!/usr/bin/env python3
+""" multirpg database sampler """
 
+import sys
 import re
 import urllib.request
+import urllib.error
 
-MYSTATS="http://multirpg.net/rawplayers2.php"
+#MYPLAYERS=["HRH_H_Crab","testcrab","macrocrab"]
+MYPLAYERS=["HRH_H_Crab"]
+STATSURL="http://multirpg.net/rawplayers3.php"
 
-# Read the data
-with urllib.request.urlopen(MYSTATS) as response:
-       html = response.read()
+def read_data(mystatsurl):
+    """ read some data """
+    try:
+        with urllib.request.urlopen(mystatsurl) as response:
+            data = response.read()
+    except urllib.error.HTTPError as error:
+        print(error.__dict__)
+        sys.exit()
+    except urllib.error.URLError as error:
+        print(error.__dict__)
+        sys.exit()
+    # Convert bytes to string
+    data=str(data, 'UTF8')
+    # Fix the classes
+    data = re.sub(r'\{[^{}]*\}', lambda x: x.group(0).replace(' ','_'), data)
+    data=data.split('\n')
+    return data
 
-# Convert bytes to string
-html=str(html, 'UTF8')
-strings=html.split('\n')
+def get_myplayer(thisdata, thisplayer):
+    """ parse some data """
+    for string in thisdata:
+        playerstats = dict(zip(string.split()[::2], string.split()[1::2]))
+        if playerstats['char'] == thisplayer:
+            return playerstats
+    return None
 
-print(strings[0])
+def sanitise_data(thesestats):
+    """ strip out stuff we don't want """
+    cleanstats = {}
+    cleanstats.update({"char":thesestats["char"]})
+    cleanstats.update({"rank":thesestats["rank"]})
+    cleanstats.update({"level":thesestats["level"]})
+    cleanstats.update({"sum":thesestats["sum"]})
+    cleanstats.update({"ttl":thesestats["ttl"]})
+    cleanstats.update({"gold":thesestats["gold"]})
+    cleanstats.update({"bank":thesestats["bank"]})
+    return cleanstats
+
+# Start here
+
+mydata = read_data(STATSURL)
+
+for myplayer in MYPLAYERS:
+    mystats = get_myplayer(mydata, myplayer)
+    mystats = sanitise_data(mystats)
+    print(mystats)
+    print()
