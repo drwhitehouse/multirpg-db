bda6241d51e7fd04ceedbeddd86228ac524eb68a
drwhitehouse <crab@dim-sum.org>
2025-01-16 17:50:49 +0000

    two graphs now...

--- read.py
+++ read.py
@@ -11,22 +11,27 @@ MY_DATABASE = "multirpg"
 MY_DB_USER = "crab"
 TABLE = "players"
 CS = f"dbname={MY_DATABASE} user={MY_DB_USER}"
-QUERY = "SELECT date, rank FROM players WHERE char = 'testcrab'"
+QUERY = "SELECT date, rank, sum FROM players WHERE char = 'macrocrab'"
 
-def plot_graph(x, y):
+def plot_graph(time_ax, rank_ax, sum_ax):
     """ plot the graph """
-    fig, ax = plt.subplots()
-    plt.xticks(rotation=90)
-    ax.plot(x, y)
-    ax.invert_yaxis()
+    fig, axs = plt.subplots(1,2)
+    axs[0].plot(time_ax, rank_ax)
+    axs[0].tick_params('x', labelrotation=45)
+    axs[0].invert_yaxis()
+    axs[0].set_title('rank')
+    axs[1].plot(time_ax, sum_ax)
+    axs[1].tick_params('x', labelrotation=45)
+    axs[1].set_title('sum')
     fig.tight_layout()
     plt.savefig('output/test.png')
     return
 
 def main():
     ''' start here '''
-    x_axis = []
-    y_axis = []
+    my_time = []
+    rank = []
+    sum = []
     print("Connecting to database...")
     try:
         conn = psycopg2.connect(CS)
@@ -36,12 +41,15 @@ def main():
     except (Exception, psycopg2.Error) as error:
         print("Error while fetching data from PostgreSQL", error)
     for row in records:
-        dt = row[0].replace(tzinfo=None) # We have to do this because numpy is deprecating timezone aware datetimes.
-        x_axis.append(dt)
-        y_axis.append(row[1])
-    my_x_array = numpy.array(x_axis, dtype='datetime64[s]')
-    my_y_array = numpy.array(y_axis)
-    plot_graph(my_x_array, my_y_array)
+        my_dt = row[0].replace(tzinfo=None) # We have to do this because numpy is deprecating timezone aware datetimes.
+        my_time.append(my_dt)
+        rank.append(row[1])
+        sum.append(row[2])
+    time_array = numpy.array(my_time, dtype='datetime64[s]')
+    rank_array = numpy.array(rank)
+    sum_array = numpy.array(sum)
+    print(sum_array)
+    plot_graph(time_array, rank_array, sum_array)
 
 if __name__ == "__main__":
     main()
