ff56e83a553210ffb2a9ce5429f09f8992816526
drwhitehouse <crab@dim-sum.org>
2025-03-27 19:54:53 +0000

    adding bkp dir

---
+++ bkp/readme.txt
@@ -0,0 +1,1 @@
+Backups go here.
--- read.py
+++ read.py
@@ -10,12 +10,29 @@ import matplotlib.pyplot as plt
 MY_DATABASE = "multirpg"
 MY_DB_USER = "crab"
 TABLE = "players"
+CHAR = "testcrab"
 CS = f"dbname={MY_DATABASE} user={MY_DB_USER}"
-QUERY = "SELECT date, rank, sum FROM players WHERE char = 'macrocrab'"
+
+def connect_db():
+    """ connect to the database """
+    print("Connecting to database...")
+    try:
+        conn = psycopg2.connect(CS)
+        cursor = conn.cursor()
+        query = sql.SQL("SELECT date, rank, sum FROM {} WHERE char = {}").format(
+                sql.Identifier(TABLE),
+                sql.Literal(CHAR),
+        )
+        cursor.execute(query)
+        records = cursor.fetchall()
+        conn.close()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+    return records
 
 def plot_graph(time_ax, rank_ax, sum_ax):
     """ plot the graph """
-    fig, axs = plt.subplots(1,2)
+    fig, axs = plt.subplots(2,1)
     axs[0].plot(time_ax, rank_ax)
     axs[0].tick_params('x', labelrotation=45)
     axs[0].invert_yaxis()
@@ -32,15 +49,8 @@ def main():
     my_time = []
     rank = []
     sum = []
-    print("Connecting to database...")
-    try:
-        conn = psycopg2.connect(CS)
-        cursor = conn.cursor()
-        cursor.execute(QUERY)
-        records = cursor.fetchall()
-    except (Exception, psycopg2.Error) as error:
-        print("Error while fetching data from PostgreSQL", error)
-    for row in records:
+    my_records = connect_db()
+    for row in my_records:
         my_dt = row[0].replace(tzinfo=None) # We have to do this because numpy is deprecating timezone aware datetimes.
         my_time.append(my_dt)
         rank.append(row[1])
@@ -48,7 +58,6 @@ def main():
     time_array = numpy.array(my_time, dtype='datetime64[s]')
     rank_array = numpy.array(rank)
     sum_array = numpy.array(sum)
-    print(sum_array)
     plot_graph(time_array, rank_array, sum_array)
 
 if __name__ == "__main__":
